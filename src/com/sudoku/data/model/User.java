7882dd1ace9eb2782c8a2970f9341b9b3abe2831
Jean-Edouard BOULANGER <JE@jeboulan-hz-1.etu.utc>
2014-10-21 16:05:00 +0200

    Creation de la classe User

--- src/com/sudoku/data/model/User.java
+++ src/com/sudoku/data/model/User.java
@@ -14,8 +14,117 @@ public class User {
     private String profilePicturePath;
     private String createDate;
     private String updateDate;
+    private String ipAdress;
     
-    
-    User(){}
-    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the pseudo
+     */
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    /**
+     * @param pseudo the pseudo to set
+     */
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    /**
+     * @return the salt
+     */
+    public String getSalt() {
+        return salt;
+    }
+
+    /**
+     * @param salt the salt to set
+     */
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    /**
+     * @return the birthdate
+     */
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    /**
+     * @param birthdate the birthdate to set
+     */
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    /**
+     * @return the profilePicturePath
+     */
+    public String getProfilePicturePath() {
+        return profilePicturePath;
+    }
+
+    /**
+     * @param profilePicturePath the profilePicturePath to set
+     */
+    public void setProfilePicturePath(String profilePicturePath) {
+        this.profilePicturePath = profilePicturePath;
+    }
+
+    /**
+     * @return the createDate
+     */
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    /**
+     * @param createDate the createDate to set
+     */
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    /**
+     * @return the updateDate
+     */
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    /**
+     * @param updateDate the updateDate to set
+     */
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    /**
+     * @return the ipAdress
+     */
+    public String getIpAdress() {
+        return ipAdress;
+    }
+
+    /**
+     * @param ipAdress the ipAdress to set
+     */
+    public void setIpAdress(String ipAdress) {
+        this.ipAdress = ipAdress;
+    }
 }
