3ebd8cd61388422df1b667ceec2303f836aca20f
Jean-Edouard BOULANGER <JE@jeboulan-hz-1.etu.utc>
2014-10-21 15:56:57 +0200

    Modification de la hierarchie package

---
+++ src/com/sudoku/data/model/User.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sudoku.data.model;
+
+public class User {
+
+    private int id;
+    private String pseudo;
+    private String salt;
+    private String birthdate;
+    private String profilePicturePath;
+    private String createDate;
+    private String updateDate;
+    
+    
+    User(){}
+    
+}
