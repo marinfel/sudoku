c116facc7ca82fa0aa98a6766cf5cdfd70e0aca5
BenFradet <benjamin.fradet@gmail.com>
2014-11-04 14:57:26 +0100

    renamed for CamelCase

---
+++ src/com/sudoku/ihm_grid/ihm_grid_cells/IhmCellView.java
@@ -0,0 +1,36 @@
+package com.sudoku.ihm_grid.ihm_grid_cells;
+import javafx.scene.text.Text;
+
+/**
+ * @class IHM_CellView
+ * this is a class to afficher the chiffer in a cell
+ * @author 
+ */
+public class IhmCellView extends IHM_Cell
+{
+    protected Text valueView;
+    protected boolean hidden;
+    
+    public IhmCellView()
+    {
+        // Init hidden attribut at false
+        hidden = false; 
+        
+        /*init valueView attribut and add it to the layout*/
+        valueView = new Text();
+        setConstraints(valueView,0,0);
+        getChildren().add(valueView);
+        System.out.println("IHM_CellView()");
+    }
+
+    @Override
+    public void setValue(int value)
+    {
+        //first check the value
+        if(checkValue(value))
+            //set the value
+            valueView.setText(String.valueOf(value));
+        
+    }
+    
+}
