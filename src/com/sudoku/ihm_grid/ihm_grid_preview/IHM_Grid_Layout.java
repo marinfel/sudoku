aa5c613d1bce11350ee4fec1bf28b2d0aecd413d
meligroud <meligroud@gmail.com>
2014-11-04 14:58:26 +0100

    Amélie est dans le moov

---
+++ src/com/sudoku/ihm_grid/ihm_grid_preview/IHM_Grid_Layout.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sudoku.ihm_grid.ihm_grid_preview;
+
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+/**
+ *
+ * @author Mélie
+ */
+abstract class IHM_Grid_Layout {
+    protected Group root;
+    protected Scene layout;
+    protected Label title;
+    protected final int sceneHeight = 200;
+    protected final int sceneLength = 200;
+    //Grid grille;
+    public IHM_Grid_Layout(String ttl){
+        root = new Group();
+        layout = new Scene(root, 200, 200);
+        title = new Label(ttl);
+        root.getChildren().add(title);
+        StackPane.setAlignment(title, Pos.TOP_CENTER);
+    }
+
+    /**
+     * @return the root
+     */
+    public Group getRoot() {
+        return root;
+    }
+
+    /**
+     * @return the layout
+     */
+    public Scene getLayout() {
+        return layout;
+    }
+
+    /**
+     * @return the title
+     */
+    public Label getTitle() {
+        return title;
+    }
+}
