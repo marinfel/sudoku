471f5fa1048436bcc4303251a7276ed906d6ea25
laetitia <leichtnaml@gmail.com>
2014-11-11 14:04:21 +0100

    "changement package ihm_grid"

--- src/com/sudoku/grid/ihm_grid_preview/IhmGridPreview.java
+++ src/com/sudoku/grid/ihm_grid_preview/IhmGridPreview.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sudoku.ihm_grid.ihm_grid_preview;
+package com.sudoku.grid.ihm_grid_preview;
+
 
 import com.sudoku.grid.ihm_grid_cells.IhmGridLines;
-import com.sudoku.ihm_grid.ihm_grid_preview.IhmGridLayout;
 import java.util.Vector;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
