74d1eba1320dd59f8879758bb8c2c23ec7e65a6a
Bqbqr <ucantest@gmail.com>
2014-11-18 14:44:54 +0100

    Take out tab and put spaces

--- src/com/sudoku/data/model/Grid.java
+++ src/com/sudoku/data/model/Grid.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Grid {
-	private int id;
-	private String title;
-	private String description;
-	private int difficulty;
-	private boolean published;
-	private List<Comment> comments;
-	private List<Tag> tags;
+    private int id;
+    private String title;
+    private String description;
+    private int difficulty;
+    private boolean published;
+    private List<Comment> comments;
+    private List<Tag> tags;
     private Cell[][] grid;
-	private User createUser;
-	private Timestamp createDate;
-	private Timestamp updateDate;
-	
+    private User createUser;
+    private Timestamp createDate;
+    private Timestamp updateDate;
+    
 
-	public Grid(){
+    public Grid(){
             comments = new ArrayList<>();
             tags = new ArrayList<>();
 
@@ -28,8 +28,8 @@ public class Grid {
                     grid[i][j] = new EmptyCell(i, j);
                 }
             }
-	}
-	
+    }
+    
         public void setEmptyCell(byte x, byte y) throws IllegalArgumentException{
             if(x < 0 || x > 9 || y < 0 || y > 9){
                 throw new IllegalArgumentException(Cell.Errors.Cell_illegal_position);
@@ -62,128 +62,128 @@ public class Grid {
             return grid[x][y];
         } 
         
-	public String getTitle(){
-		return title;
-	}
-	
-	public void setTitle(String titre){
-		this.title=titre;
-	}
+    public String getTitle(){
+        return title;
+    }
+    
+    public void setTitle(String titre){
+        this.title=titre;
+    }
         
-	public int getMeanGrades(){
-		int i=0,result=0;
-		for(Comment comment:comments){
-			result+=comment.getGrade();
-			i++;
-		}
-		
-		return result/i;
-	}
+    public int getMeanGrades(){
+        int i=0,result=0;
+        for(Comment comment:comments){
+            result+=comment.getGrade();
+            i++;
+        }
+        
+        return result/i;
+    }
     public int getId() {
-		return id;
-	}
-	
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public int getDifficulty() {
-		return difficulty;
-	}
-	public void setDifficulty(int difficulty) {
-		this.difficulty = difficulty;
-	}
-	public boolean isPublished() {
-		return published;
-	}
-	public void setPublished(boolean published) {
-		this.published = published;
-	}
-	public List<Comment> getComments() {
-		return comments;
-	}
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
-	}
-	public List<Tag> getTags() {
-		return tags;
-	}
-	public void setTags(List<Tag> tags) {
-		this.tags = tags;
-	}
-	public User getCreateUser() {
-		return createUser;
-	}
-	public void setCreateUser(User createUser) {
-		this.createUser = createUser;
-	}
-	public Timestamp getCreateDate() {
-		return createDate;
-	}
-	public void setCreateDate(Timestamp createDate) {
-		this.createDate = createDate;
-	}
-	public Timestamp getUpdateDate() {
-		return updateDate;
-	}
-	public void setUpdateDate(Timestamp updateDate) {
-		this.updateDate = updateDate;
-	}
-	
-	public double getAverageGrade(){
-		double averageGrade = 0.0;
-		for(Comment comment : comments){
-			averageGrade += comment.getGrade();
-		}
-		return averageGrade / comments.size();
-	}
-	
-	public void addTag(Tag tag){
-		if(tag != null && tag.getName() != null && !tag.getName().isEmpty())
-		{
-			for(Tag t : tags){
-				if(t.getName().equals(tag.getName())){
-					return;
-				}
-			}
-			tags.add(tag);
-		}
-	}
-	
-	public void removeTag(Tag tag){
-		if(tag != null && tag.getName() != null && !tag.getName().isEmpty())
-		{
-			for(Tag t : tags){
-				if(t.getName().equals(tag.getName())){
-					tags.remove(t);
-					return;
-				}
-			}
-		}
-	}
-	
-	public void addComment(Comment comment){
-		if(comment != null && comment.getComment() != null && !comment.getComment().isEmpty())
-		{
-			comments.add(comment);
-		}
-	}
-	
-	public void removeComment(Comment comment){
-		if(comment != null && comment.getComment() != null && !comment.getComment().isEmpty())
-		{
-			for(Comment c : comments){
-				if(c.getComment().equals(comment.getComment()) && c.getGrade() == comment.getGrade()){
-					comments.remove(c);
-					return;
-				}
-			}
-		}		
-	}
+        return id;
+    }
+    
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getDifficulty() {
+        return difficulty;
+    }
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+    public boolean isPublished() {
+        return published;
+    }
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+    public List<Comment> getComments() {
+        return comments;
+    }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+    public User getCreateUser() {
+        return createUser;
+    }
+    public void setCreateUser(User createUser) {
+        this.createUser = createUser;
+    }
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+    }
+    
+    public double getAverageGrade(){
+        double averageGrade = 0.0;
+        for(Comment comment : comments){
+            averageGrade += comment.getGrade();
+        }
+        return averageGrade / comments.size();
+    }
+    
+    public void addTag(Tag tag){
+        if(tag != null && tag.getName() != null && !tag.getName().isEmpty())
+        {
+            for(Tag t : tags){
+                if(t.getName().equals(tag.getName())){
+                    return;
+                }
+            }
+            tags.add(tag);
+        }
+    }
+    
+    public void removeTag(Tag tag){
+        if(tag != null && tag.getName() != null && !tag.getName().isEmpty())
+        {
+            for(Tag t : tags){
+                if(t.getName().equals(tag.getName())){
+                    tags.remove(t);
+                    return;
+                }
+            }
+        }
+    }
+    
+    public void addComment(Comment comment){
+        if(comment != null && comment.getComment() != null && !comment.getComment().isEmpty())
+        {
+            comments.add(comment);
+        }
+    }
+    
+    public void removeComment(Comment comment){
+        if(comment != null && comment.getComment() != null && !comment.getComment().isEmpty())
+        {
+            for(Comment c : comments){
+                if(c.getComment().equals(comment.getComment()) && c.getGrade() == comment.getGrade()){
+                    comments.remove(c);
+                    return;
+                }
+            }
+        }        
+    }
 }
--- src/com/sudoku/data/model/PlayedGrid.java
+++ src/com/sudoku/data/model/PlayedGrid.java
@@ -7,50 +7,50 @@ import java.util.List;
 
 
 public class PlayedGrid {
-	private Grid grid;
-	private User player;
-	List<CellGuess> guessedCells;
-	Timestamp startDate;
-	Timestamp updateDate;
-	
-	
-	public Grid getGrid() {
-		return grid;
-	}
-	public void setGrid(Grid grid) {
-		this.grid = grid;
-	}
+    private Grid grid;
+    private User player;
+    List<CellGuess> guessedCells;
+    Timestamp startDate;
+    Timestamp updateDate;
+    
+    
+    public Grid getGrid() {
+        return grid;
+    }
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
 
-	
-	public boolean isComplete() {
-		return false;
-	}
-	public int nbRemainingCells(){
-		return 0;
-	}
+    
+    public boolean isComplete() {
+        return false;
+    }
+    public int nbRemainingCells(){
+        return 0;
+    }
 
-	public List<CellGuess> getGuessedCells() {
-		return null;
-	}
-	
-	
-	public User getPlayer() {
-		return player;
-	}
-	
-	
-	public void setPlayer(User player) {
-		this.player = player;
-	}
-	
-	public PlayedGrid(Grid grid) {
-		super();
-		this.grid = grid;
-	}
-	
-	public PlayedGrid(Grid grid, User player) {
-		super();
-		this.grid = grid;
-		this.player = player;
-	}
+    public List<CellGuess> getGuessedCells() {
+        return null;
+    }
+    
+    
+    public User getPlayer() {
+        return player;
+    }
+    
+    
+    public void setPlayer(User player) {
+        this.player = player;
+    }
+    
+    public PlayedGrid(Grid grid) {
+        super();
+        this.grid = grid;
+    }
+    
+    public PlayedGrid(Grid grid, User player) {
+        super();
+        this.grid = grid;
+        this.player = player;
+    }
 }
