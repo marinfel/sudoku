7d09629c453fcd699414491e494bf75257fbb500
BenFradet <benjamin.fradet@gmail.com>
2014-11-21 13:09:43 +0100

    cleared commMain

--- src/main/java/com/sudoku/comm/CommMain.java
+++ src/main/java/com/sudoku/comm/CommMain.java
@@ -4,13 +4,5 @@ package com.sudoku.comm;
  * Created by ben on 19/11/14.
  */
 public class CommMain {
-  public static void main(String[] args) {
-    NodeExplorerImpl nodeExplorer = new NodeExplorerImpl();
-    try {
-      System.out.println(
-          nodeExplorer.getLocalInetAddress().getHostAddress());
-    } catch(Exception ex) {
-
-    }
-  }
+  public static void main(String[] args) { }
 }
