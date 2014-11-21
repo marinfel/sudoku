be4d95d218ce1ff875cfe5fdb4d604293853f5d3
BenFradet <benjamin.fradet@gmail.com>
2014-11-21 13:10:18 +0100

    refactored nodeExplorerImpl

--- src/main/java/com/sudoku/comm/NodeExplorerImpl.java
+++ src/main/java/com/sudoku/comm/NodeExplorerImpl.java
@@ -15,73 +15,27 @@ import java.util.Enumeration;
  * Created by ben on 19/11/14.
  */
 public class NodeExplorerImpl implements NodeExplorer {
-  private Logger logger = LoggerFactory.getLogger(NodeExplorerImpl.class);
+  private CommunicationManager commManager;
+
+  public NodeExplorerImpl() {
+    commManager = CommunicationManager.getInstance();
+  }
 
   @Override
   public Message discoverNode(Message sentMessage) throws AvroRemoteException {
-    String localIp = null;
-    try {
-      localIp = getLocalInetAddress().getHostAddress();
-    } catch (UnknownHostException ex) {
-      logger.error(ex.toString());
-    }
-
-    CommunicationManager.setConnectedIps(CollectionUtil.union(
-        CommunicationManager.getConnectedIps(), sentMessage.getListIps()));
-    CommunicationManager.getConnectedIps().remove(localIp);
+    commManager.setConnectedIps(CollectionUtil.merge(
+        commManager.getConnectedIps(), sentMessage.getListIps(),
+        commManager.getLocalIp()));
     return Message.newBuilder()
-        .setListIps(CommunicationManager.getConnectedIps())
-        .setLogin(CommunicationManager.LOGIN)
-        .setUuid(CommunicationManager.UUID)
+        .setListIps(commManager.getConnectedIps())
+        .setLogin(commManager.getLogin())
+        .setUuid(commManager.getUuid())
         .build();
   }
 
   @Override
   public Void disconnect(String ip) throws AvroRemoteException {
-    CommunicationManager.getConnectedIps().remove(ip);
+    commManager.getConnectedIps().remove(ip);
     return null;
-  }
-
-  public InetAddress getLocalInetAddress() throws UnknownHostException {
-    try {
-      InetAddress candidateAddress = null;
-
-      for (Enumeration<NetworkInterface> networkInterfaces =
-               NetworkInterface.getNetworkInterfaces();
-           networkInterfaces.hasMoreElements();) {
-
-        NetworkInterface networkInterface =
-            (NetworkInterface) networkInterfaces.nextElement();
-        for (Enumeration<InetAddress> inetAddresses =
-                 networkInterface.getInetAddresses();
-            inetAddresses.hasMoreElements();) {
-
-          InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
-          if (!inetAddress.isLoopbackAddress()) {
-            if (inetAddress.isSiteLocalAddress()) {
-              return inetAddress;
-            } else if (candidateAddress == null) {
-              candidateAddress = inetAddress;
-            }
-          }
-        }
-      }
-
-      if (candidateAddress != null) {
-        return candidateAddress;
-      }
-
-      InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
-      if(jdkSuppliedAddress == null) {
-        throw new UnknownHostException("InetAddress.getLocalHost() is null.");
-      }
-      return jdkSuppliedAddress;
-    } catch (Exception ex) {
-      logger.error(ex.toString());
-      UnknownHostException unknownHostException =
-          new UnknownHostException("Failed to determine IP: " + ex);
-      unknownHostException.initCause(ex);
-      throw unknownHostException;
-    }
   }
 }
