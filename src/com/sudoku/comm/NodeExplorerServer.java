13f50b2b41c0636b70881e466d125bf26d2a8d8b
BenFradet <benjamin.fradet@gmail.com>
2014-11-26 22:15:32 +0100

    renamed PORT in EXPLORER_PORT in Server

--- src/com/sudoku/comm/NodeExplorerServer.java
+++ src/com/sudoku/comm/NodeExplorerServer.java
@@ -2,13 +2,11 @@ package com.sudoku.comm;
 
 import com.sudoku.comm.generated.NodeExplorer;
 import org.apache.avro.ipc.NettyServer;
-import org.apache.avro.ipc.Server;
 import org.apache.avro.ipc.specific.SpecificResponder;
 
 import java.net.InetSocketAddress;
 
-public class NodeExplorerServer extends SudokuServer {
-  private static Server server;
+public class NodeExplorerServer extends Server {
 
   public NodeExplorerServer() {
     super();
@@ -17,7 +15,7 @@ public class NodeExplorerServer extends SudokuServer {
   public void startServer() {
     server = new NettyServer(
         new SpecificResponder(NodeExplorer.class, new NodeExplorerImpl()),
-        new InetSocketAddress(localIp, PORT)
+        new InetSocketAddress(localIp, EXPLORER_PORT)
     );
   }
 
