71848f0e87b85280da1dedeb01a0d7ef907697cb
BenFradet <benjamin.fradet@gmail.com>
2014-12-01 23:12:53 +0100

    implemented getGrids and getProfiles in the CommunicationManager

--- src/com/sudoku/comm/CommunicationManager.java
+++ src/com/sudoku/comm/CommunicationManager.java
@@ -1,15 +1,9 @@
 package com.sudoku.comm;
 
-import com.sudoku.comm.generated.Message;
-import com.sudoku.comm.generated.NodeExplorer;
-import com.sudoku.data.model.Comment;
-import com.sudoku.data.model.Grid;
-import com.sudoku.data.model.User;
+import com.sudoku.comm.generated.*;
 import com.sudoku.util.CollectionUtil;
 import org.apache.avro.ipc.NettyTransceiver;
 import org.apache.avro.ipc.specific.SpecificRequestor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -23,10 +17,12 @@ public final class CommunicationManager {
   private String login;
   private ArrayList<String> connectedIps;
   private Server nodeExplorerServer;
+  private Server dataRetrieverServer;
 
   private CommunicationManager() {
     super();
     nodeExplorerServer = new NodeExplorerServer();
+    dataRetrieverServer = new DataRetrieverServer();
   }
 
   public final static CommunicationManager getInstance() {
@@ -50,6 +46,7 @@ public final class CommunicationManager {
 
   private void startServer() {
     nodeExplorerServer.startServer();
+    dataRetrieverServer.startServer();
   }
 
   public void discoverNodes() throws IOException {
@@ -86,23 +83,82 @@ public final class CommunicationManager {
         client.close();
       }
     }
+    dataRetrieverServer.stopServer();
     nodeExplorerServer.stopServer();
   }
 
-  public ArrayList<Grid> getAllGrids() {
+  public ArrayList<com.sudoku.data.model.Grid> getAllGrids()
+      throws IOException {
+    ArrayList<com.sudoku.data.model.Grid> resultGrids = new ArrayList<>();
+    if (connectedIps != null) {
+      ArrayList<Grid> grids = new ArrayList<>();
+      for (String ip : connectedIps) {
+        NettyTransceiver client =
+            new NettyTransceiver(new InetSocketAddress(ip, PORT));
+        DataRetriever retriever = (DataRetriever)
+            SpecificRequestor.getClient(DataRetriever.class, client);
+        grids.addAll(retriever.getGrids());
+      }
+      for (Grid grid : grids) {
+        resultGrids.add(com.sudoku.data.model.Grid.buildFromAvroGrid(grid));
+      }
+    }
+    return resultGrids;
+  }
+
+  public ArrayList<com.sudoku.data.model.Grid> getAllGrids(
+      com.sudoku.data.model.User user) throws IOException {
+    return getAllGrids(user.getIpAddress());
+  }
+
+  public ArrayList<com.sudoku.data.model.Grid> getAllGrids(String ip)
+      throws IOException {
+    ArrayList<com.sudoku.data.model.Grid> grids = new ArrayList<>();
+    if (ip != null) {
+      NettyTransceiver client =
+          new NettyTransceiver(new InetSocketAddress(ip, PORT));
+      DataRetriever retriever = (DataRetriever)
+          SpecificRequestor.getClient(DataRetriever.class, client);
+      for (Grid grid : retriever.getGrids()) {
+        grids.add(com.sudoku.data.model.Grid.buildFromAvroGrid(grid));
+      }
+    }
+    return grids;
+  }
+
+  public ArrayList<com.sudoku.data.model.User> getAllProfiles()
+      throws IOException {
+    ArrayList<com.sudoku.data.model.User> users = new ArrayList<>();
+    if (connectedIps != null) {
+      for (String ip : connectedIps) {
+        NettyTransceiver client =
+            new NettyTransceiver(new InetSocketAddress(ip, PORT));
+        DataRetriever retriever = (DataRetriever)
+            SpecificRequestor.getClient(DataRetriever.class, client);
+        users.add(com.sudoku.data.model.User
+            .buildFromAvroUser(retriever.getProfile()));
+      }
+    }
+    return users;
+  }
+
+  public com.sudoku.data.model.User getProfile(User user) throws IOException {
+    return getProfile(user.getIpAddress());
+  }
+
+  public com.sudoku.data.model.User getProfile(String ip) throws IOException {
+    if (ip != null) {
+      NettyTransceiver client =
+          new NettyTransceiver(new InetSocketAddress(ip, PORT));
+      DataRetriever retriever = (DataRetriever)
+          SpecificRequestor.getClient(DataRetriever.class, client);
+      return com.sudoku.data.model.User
+          .buildFromAvroUser(retriever.getProfile());
+    }
     return null;
   }
 
-  public ArrayList<Grid> getAllGrids(User user) {
-    return null;
-  }
-
-  public ArrayList<User> getAllProfiles() {
-    return null;
-  }
-
-  public void pushCommentAndSync(Comment newComment, Grid gridToSync) {
-  }
+  public void pushCommentAndSync(Comment newComment, Grid gridToSync) { }
 
   public ArrayList<String> getConnectedIps() {
     return connectedIps;
