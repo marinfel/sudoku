2be4f22e1bd24922918a992fd44f75cff3191f0c
BenFradet <benjamin.fradet@gmail.com>
2014-12-01 16:51:53 +0100

    done implementing DataRetriever

--- src/com/sudoku/comm/DataRetrieverImpl.java
+++ src/com/sudoku/comm/DataRetrieverImpl.java
@@ -1,10 +1,13 @@
 package com.sudoku.comm;
 
+import com.sudoku.comm.generated.Comment;
 import com.sudoku.comm.generated.DataRetriever;
 import com.sudoku.comm.generated.Grid;
 import com.sudoku.comm.generated.User;
 import com.sudoku.data.manager.GridManager;
 import com.sudoku.data.manager.UserManager;
+import com.sudoku.data.model.Cell;
+import com.sudoku.data.model.FixedCell;
 import org.apache.avro.AvroRemoteException;
 
 import java.util.ArrayList;
@@ -18,10 +21,50 @@ public class DataRetrieverImpl implements DataRetriever {
   public List<Grid> getGrids() throws AvroRemoteException {
     List<com.sudoku.data.model.Grid> availableGrids =
         GridManager.getInstance().getAvailableGrids();
-    ArrayList<Grid> grids = new ArrayList<Grid>();
+    ArrayList<Grid> grids = new ArrayList<>();
     for (com.sudoku.data.model.Grid grid : availableGrids) {
-      //grids.add(Grid.newBuilder()
-      //  .set)
+      ArrayList<Comment> comments = new ArrayList<>();
+      for (com.sudoku.data.model.Comment comment : grid.getComments()) {
+        comments.add(Comment.newBuilder()
+            .setAuthor(
+                com.sudoku.data.model.User.buildAvroUser(comment.getAuthor()))
+            .setComment(comment.getComment())
+            .setGrade(comment.getGrade())
+            .build());
+      }
+
+      ArrayList<String> tags = new ArrayList<>();
+      for (com.sudoku.data.model.Tag tag : grid.getTags()) {
+        tags.add(tag.getName());
+      }
+
+      List<List<Integer>> resultMatrix = new ArrayList<>();
+      Cell[][] matrix = grid.getGrid();
+      for (byte i = 0; i < matrix.length; i++) {
+        resultMatrix.add(i, new ArrayList<Integer>());
+        for (byte j = 0; j < matrix[0].length; j++) {
+          if (matrix[i][j] instanceof FixedCell) {
+            resultMatrix.get(i).add(j,
+                (int)((FixedCell) matrix[i][j]).getValue());
+          } else {
+            resultMatrix.get(i).add(j, null);
+          }
+        }
+      }
+
+      grids.add(Grid.newBuilder()
+          .setCreateDate(grid.getCreateDate().toString())
+          .setUpdateDate(grid.getUpdateDate().toString())
+          .setId(grid.getId())
+          .setTitle(grid.getTitle())
+          .setDescription(grid.getDescription())
+          .setDifficulty(grid.getDifficulty())
+          .setCreateUser(
+              com.sudoku.data.model.User.buildAvroUser(grid.getCreateUser()))
+          .setComments(comments)
+          .setTags(tags)
+          .setMatrix(resultMatrix)
+          .build());
     }
     return grids;
   }
@@ -30,13 +73,6 @@ public class DataRetrieverImpl implements DataRetriever {
   public User getProfile() throws AvroRemoteException {
     com.sudoku.data.model.User currentUser =
         UserManager.getInstance().getLoggedUser();
-    return User.newBuilder()
-        .setBirthDate(currentUser.getBirthDate().toString())
-        .setCreateDate(currentUser.getCreateDate().toString())
-        .setIpAddress(currentUser.getIpAddress())
-        .setProfilePicturePath(currentUser.getProfilePicturePath())
-        .setPseudo(currentUser.getPseudo())
-        .setUpdateDate(currentUser.getUpdateDate().toString())
-        .build();
+    return com.sudoku.data.model.User.buildAvroUser(currentUser);
   }
 }
