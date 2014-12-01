7dd7638cd4f54d880a1df0fbe7fc5a3b29d45004
BenFradet <benjamin.fradet@gmail.com>
2014-12-01 23:12:19 +0100

    DataRetrieverServer class

---
+++ src/com/sudoku/comm/DataRetrieverServer.java
@@ -0,0 +1,30 @@
+package com.sudoku.comm;
+
+import com.sudoku.comm.generated.DataRetriever;
+import com.sudoku.comm.generated.NodeExplorer;
+import org.apache.avro.ipc.NettyServer;
+import org.apache.avro.ipc.specific.SpecificResponder;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by ben on 01/12/14.
+ */
+public class DataRetrieverServer extends Server {
+  private final int PORT = 11024;
+
+  @Override
+  public void startServer() {
+    server = new NettyServer(
+        new SpecificResponder(DataRetriever.class, new DataRetrieverImpl()),
+        new InetSocketAddress(localIp, PORT)
+    );
+  }
+
+  @Override
+  public void stopServer() {
+    if (server != null) {
+      server.close();
+    }
+  }
+}
