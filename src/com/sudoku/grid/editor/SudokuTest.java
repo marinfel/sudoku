1f8faab2d1139b74554ab07d913e8431fb007b7f
CelineTo <celine.to.utc@gmail.com>
2014-12-02 15:57:09 +0100

    Merge origin/master
    Conflicts:
    	src/com/sudoku/grid/editor/SudokuTest.java

--- src/com/sudoku/grid/editor/SudokuTest.java
+++ src/com/sudoku/grid/editor/SudokuTest.java
@@ -5,6 +5,8 @@
  */
 package com.sudoku.grid.editor;
 
+import com.sudoku.data.model.Grid;
+import com.sudoku.grid.ihm_grid_player.IhmGridPlayer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -24,11 +26,11 @@ public class SudokuTest extends Application {
   @Override
   public void start(Stage primaryStage) {
 
-    IhmGridEditorRandomlyFilled ihm_test;
+    //IhmGridEditorRandomlyFilled ihm_test;
     IhmGridEditorManuallyFilled ihm_test2;
     IhmGridPlayer ihm_test3;
 
-    ihm_test = new IhmGridEditorRandomlyFilled();
+    //ihm_test = new IhmGridEditorRandomlyFilled();
     ihm_test2 = new IhmGridEditorManuallyFilled();
     ihm_test3 = new IhmGridPlayer(new Grid());
     //Scene scene = new Scene(ihm_test, 800, 1000);
