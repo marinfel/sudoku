e512d94f140c3bf74a524575fcabc3d178a790b0
mouradsaadi <blackstonedrummer@gmail.com>
2014-12-12 18:22:51 +0100

    Update listgridmanager for IHM Main

--- src/com/sudoku/main/manager/ListGridManager.java
+++ src/com/sudoku/main/manager/ListGridManager.java
@@ -5,6 +5,7 @@
  */
 package com.sudoku.main.manager;
 
+import com.sudoku.data.manager.GridManager;
 import com.sudoku.data.model.Grid;
 import com.sudoku.data.sample.DataSample;
 import java.util.ArrayList;
@@ -37,11 +38,15 @@ public class ListGridManager {
     public GridPane getGridThumbnailContainer()
     {
         GridThumbnailContainer container = new GridThumbnailContainer();
-        container.addGridThumbnail(new GridThumbnail(instance.g1));
+        /*container.addGridThumbnail(new GridThumbnail(instance.g1));
         container.addGridThumbnail(new GridThumbnail(instance.g2));
         container.addGridThumbnail(new GridThumbnail(instance.g3));
         container.addGridThumbnail(new GridThumbnail(instance.g1));
-        container.addGridThumbnail(new GridThumbnail(instance.g2));
+        container.addGridThumbnail(new GridThumbnail(instance.g2));*/
+        for(int i = 0; i<GridManager.getInstance().getUserGrids(instance.a).size();i++)
+        {
+            container.addGridThumbnail(new GridThumbnail(GridManager.getInstance().getUserGrids(instance.a).get(i)));
+        }
         return container.getInstance();
     }
 }
