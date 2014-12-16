681a9d0397a6a78691ccac9e947241723e967573
clesaege <clemage@live.fr>
2014-12-16 15:38:59 +0100

    Update Comment.java

--- src/com/sudoku/data/model/Comment.java
+++ src/com/sudoku/data/model/Comment.java
@@ -1,5 +1,10 @@
 package com.sudoku.data.model;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +15,7 @@ package com.sudoku.data.model;
  * @author jonathan
  */
 
-public class Comment {
+public final class Comment {
   /**
    * Champs de la classe Comment
    * *
@@ -20,7 +25,7 @@ public class Comment {
   private final String userSalt; // We use the salt as an UUID to identify the poster
   private String comment;
   private Integer grade; // Number of half stars. If some comment is graded 3 stars and a half, grade will be 7.
-
+  private Date creationDate;
   /**
    * Méthode de la classe  Comment
      * @param comment
@@ -39,6 +44,13 @@ public class Comment {
   public Comment(String comment, Double grade, User u) {
     this.comment = comment;
     this.setGrade(grade);
+    this.author = u;
+    this.pseudo = u.getPseudo();
+    this.userSalt = u.getSalt();
+  }
+ public Comment(String comment, Integer grade, User u) {
+    this.comment = comment;
+    this.grade=grade;
     this.author = u;
     this.pseudo = u.getPseudo();
     this.userSalt = u.getSalt();
@@ -66,10 +78,17 @@ public class Comment {
     this.comment = comment;
   }
 
-  public Double getGrade() { //Return the number of Stars (and not half stars)
-    return grade/2.0;
+  public Double getStarGrade() { //Return the number of Stars (and not half stars)
+    return this.grade/2.0;
+  }
+  public Integer getGrade() { //Return the number of half stars
+    return this.grade;
   }
 
+ public void setGrade(Integer grade){
+      this.grade=grade;
+  }
+  
   public void setGrade(Double grade) {
     Double halfStarGrade = grade * 2; //Set this number in halfstars
     this.grade = halfStarGrade.intValue();
@@ -77,5 +96,8 @@ public class Comment {
   public String getUserSalt(){
       return this.userSalt;
   }
-
+  public void setCreationDateNow(){
+      Calendar cal = new GregorianCalendar();
+      this.creationDate=cal.getTime();
+  }
 }
