47e44bb037a2dd3c1acf6df736e75798b02d8c4c
lleichtn <leichtnam@gmail.com>
2014-12-16 15:39:04 +0100

    fixed sonarqube issur sur module star et starbox

--- src/com/sudoku/grid/preview/StarsBox.java
+++ src/com/sudoku/grid/preview/StarsBox.java
@@ -1,35 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * StarsBox est la classe representant l objet graphique Liste d etoiles. 
+ * Elle peut etre dynamique ou statique selon l utilisation
+ * 
  */
+
 package com.sudoku.grid.preview;
 
 import java.util.ArrayList;
+import java.util.List;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 
 /**
- *
- * @author Marc-Antoine
+ * @author marco, groudame, lleichtn
  */
 public class StarsBox extends HBox implements EventHandler<MouseEvent> {
 
-  protected final int maxNumberOfStars;
-  protected ArrayList<StarView> stars = new ArrayList<>();
-  protected double valueAtClick = 0;
+    protected final int maxNumberOfStars;
 
-  public StarsBox(int numberOfStars) {
-    maxNumberOfStars = numberOfStars;
-    stars.ensureCapacity(maxNumberOfStars);
+    protected List<StarView> stars = new ArrayList<>();
 
-    //Ajoute une étoile jaune (remplie)
-    for (int i = 0; i < maxNumberOfStars; i++) {
-      stars.add(new StarView());
-      getChildren().add(stars.get(i));
+    protected double valueAtClick = 0;
+
+    /**
+     * Constructeur de l'objet liste d etoile pour l' affichage de
+     * la note moyenne d' une grille ou pour faire une notation
+     * @param numberOfStars
+     */
+    public StarsBox(int numberOfStars) {
+        
+      maxNumberOfStars = numberOfStars;
+
+      for (int i = 0; i < maxNumberOfStars; i++) {
+        stars.add(new StarView());
+        getChildren().add(stars.get(i));
+      }
     }
-  }
 
   @Override
   public void handle(MouseEvent t) {
@@ -47,22 +54,30 @@ public class StarsBox extends HBox implements EventHandler<MouseEvent> {
     }
   }
 
+  /**
+   * Cette fonction permet de 
+   * @param value
+   */
+    
   public void setValue(double value) {
+      
+    double newValue = value;
+    
     if (value < 0) {
-      value = 0;
+      newValue = 0;
     }
 
     if (value > maxNumberOfStars) {
-      value = maxNumberOfStars;
+      newValue = maxNumberOfStars;
     }
 
     int i = 0;
-    //add yallow stars
-    for (; i < Math.floor(value); i++) {
+   
+    for (; i < Math.floor(newValue); i++) {
       stars.get(i).setType(StarView.StarTypes.FILLED);
     }
 
-    if (Math.round(value) != Math.floor(value)) {
+    if (Math.round(newValue) != Math.floor(newValue)) {
       stars.get(i++).setType(StarView.StarTypes.HALF);
     }
 
@@ -71,6 +86,10 @@ public class StarsBox extends HBox implements EventHandler<MouseEvent> {
       stars.get(i).setType(StarView.StarTypes.EMPTY);
     }
   }
+  /* 
+   * Permet de rendre l objet dynamique et autoriser les handlers. 
+   * @param hoverable 
+   */
 
   public void setHoverable(boolean hoverable) {
     if (hoverable) {
@@ -84,10 +103,17 @@ public class StarsBox extends HBox implements EventHandler<MouseEvent> {
     }
   }
 
+  /**
+   * Retourne la valeur selectionne lors du clic de souris
+   * @return valueAtClick
+   */
   public double getValueAtClick() {
     return valueAtClick;
   }
 
+  /**
+   * Reset la valeur de valueAtClick ^pour permettre une nouvelle selection.
+   */
   public void reset() {
     setValue(0);
     valueAtClick = 0;
