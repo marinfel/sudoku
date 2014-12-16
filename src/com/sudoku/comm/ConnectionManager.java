b375b0e7991861cd720d62f0712a55eb673c289b
BenFradet <benjamin.fradet@gmail.com>
2014-12-16 15:30:04 +0100

    javadoc for ConnectionManager

--- src/com/sudoku/comm/ConnectionManager.java
+++ src/com/sudoku/comm/ConnectionManager.java
@@ -3,7 +3,11 @@ package com.sudoku.comm;
 import java.util.ArrayList;
 import java.util.List;
 
-// Class representing a connection to a remote user
+/**
+ * Class representing a connection to a remote user
+ * @author Murat Cansiz
+ * @see com.sudoku.comm.AvroConnectionManager
+ */
 public abstract class ConnectionManager {
   private static final String ERROR_MSG = "Must open connection first.";
   // Remote user.
@@ -12,6 +16,10 @@ public abstract class ConnectionManager {
   // should retrieve it from the communication manager or another conf singleton
   protected static final int NODEPORT = 11023;
 
+  /**
+   * Class constructor
+   * @param ip ip of the connection
+   */
   public ConnectionManager(String ip) {
     this.ipAddress = ip;
     this.isConnected = false;
@@ -22,7 +30,14 @@ public abstract class ConnectionManager {
    * @throws com.sudoku.comm.ConnectionManager.OfflineUserException
    */
   public abstract void openConnection() throws OfflineUserException;
-  
+
+  /**
+   * Gets the list of distant connected ips
+   * @param newConnectedIps connected ips to send to the distant connection
+   * @return a list of connected ips
+   * @throws ConnectionClosedException
+   * @throws OfflineUserException
+   */
   public List<String> getConnectedIps(List<String> newConnectedIps)
      throws OfflineUserException, ConnectionClosedException {
     if (!isConnected) {
@@ -30,7 +45,13 @@ public abstract class ConnectionManager {
     }
     return new ArrayList<>();
   }
-  
+
+  /**
+   * Publishes the current list of connected ips to the distant connection
+   * @param ips list of ips to be published
+   * @throws OfflineUserException
+   * @throws ConnectionClosedException
+   */
   public void publishIps(List<String> ips) throws OfflineUserException,
      ConnectionClosedException {
     if (!isConnected) {
@@ -38,6 +59,11 @@ public abstract class ConnectionManager {
     }
   }
 
+  /**
+   * Disconnects this peer to the distant peer
+   * @throws OfflineUserException
+   * @throws ConnectionClosedException
+   */
   public void disconnect() throws OfflineUserException,
      ConnectionClosedException {
     if (!isConnected) {
@@ -45,9 +71,15 @@ public abstract class ConnectionManager {
     }
   }
 
+  /**
+   * Closes connection with the distant peer
+   * @throws OfflineUserException
+   */
   public abstract void closeConnection() throws OfflineUserException;
 
-  //Exceptions
+  /**
+   * Exception class occurring when a connection closed unexpectedly
+   */
   public class ConnectionClosedException extends Exception {
     public ConnectionClosedException() {
       super();
@@ -63,6 +95,9 @@ public abstract class ConnectionManager {
     }
   }
 
+  /**
+   * Exception class occurring when a user is unexpectedly offline
+   */
   public class OfflineUserException extends Exception {
     public OfflineUserException() {
       super();
