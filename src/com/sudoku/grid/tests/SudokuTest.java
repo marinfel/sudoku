54b93a29cc311946e77289601c82f10053091f8c
CelineTo <celine.to.utc@gmail.com>
2014-12-17 18:32:18 +0100

    Merge origin/master
    Conflicts:
    	src/com/sudoku/grid/editor/SudokuTest.java

--- src/com/sudoku/grid/tests/SudokuTest.java
+++ src/com/sudoku/grid/tests/SudokuTest.java
@@ -11,13 +11,19 @@ import com.sudoku.data.manager.UserManager;
 import com.sudoku.data.model.Grid;
 import com.sudoku.data.model.Tag;
 import com.sudoku.data.model.User;
+import com.sudoku.grid.editor.IhmGridEditorManuallyFilled;
+import com.sudoku.grid.editor.IhmGridEditorRandomlyFilled;
 import com.sudoku.grid.manager.IhmGridLayout;
 import com.sudoku.grid.preview.IhmGridDetailledPreview;
+import com.sudoku.grid.player.IhmGridPlayer;
+import com.sudoku.grid.popups.IhmPopupsList;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * @author Mehdi KANE, Céline TO, This is a test class for grid ihm (editor,
@@ -35,18 +41,22 @@ public class SudokuTest extends Application {
   @Override
   public void start(Stage primaryStage) {
 
-    IhmGridLayout ihm_test;
+    IhmGridLayout ihm_test1;
+    IhmGridLayout ihm_test2;
+    IhmGridLayout ihm_test3;
+    IhmGridLayout ihm_test4;
+    IhmGridLayout ihm_test5;
 
-    //ihm_test = new IhmGridEditorRandomlyFilled();
-    //ihm_test = new IhmGridEditorManuallyFilled();
-    //ihm_test = new IhmGridPlayer(generateRandomGrid(20));
-    //ihm_test = new IhmGridPreview(2, new Grid("", UserManager.getInstance().getLoggedUser()), 100);
+    ihm_test1 = new IhmGridEditorRandomlyFilled();
+    ihm_test2 = new IhmGridEditorManuallyFilled();
+    ihm_test3 = new IhmGridPlayer(generateRandomGrid(20));
+    //ihm_test4 = new IhmGridPreview(2, new Grid("", UserManager.getInstance().getLoggedUser()), 100);
+
     Grid g = generateRandomGrid();
     ArrayList<Tag> tagsList = new ArrayList<Tag>();
     tagsList.add(new Tag("tag1"));
     tagsList.add(new Tag("tag2"));
     g.setTags(tagsList);
-
     List<Comment> commentsList = new ArrayList<Comment>();
     Comment c1 = new Comment();
     c1.setComment("Comment1");
@@ -55,30 +65,31 @@ public class SudokuTest extends Application {
     commentsList.add(c1);
     commentsList.add(c2);
     g.setComments(commentsList);
-
     User user = UserManager.getInstance().getLoggedUser();
     g.setCreateUser(user);
-
     g.setTitle("test title");
-    ihm_test = new IhmGridDetailledPreview(g, 200);
-    Scene scene = new Scene(ihm_test, 300, 300);
+    ihm_test5 = new IhmGridDetailledPreview(g, 200);
+
+    //Scene scene = new Scene(ihm_test1 800, 1000);
     //Scene scene = new Scene(ihm_test2, 800, 1000);
     //Scene scene = new Scene(ihm_test3, 800, 1000);
+    //Scene scene = new Scene(ihm_test4, 800, 1000);
+    Scene scene = new Scene(ihm_test5, 300, 300);
 
     primaryStage.setTitle("Sudoku preview");
     primaryStage.setScene(scene);
     primaryStage.show();
 
-//    primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-//
-//      @Override
-//      public void handle(WindowEvent t) {
+    primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+      @Override
+      public void handle(WindowEvent t) {
 //        if (t.getEventType() == WindowEvent.WINDOW_CLOSE_REQUEST) {
 //          IhmPopupsList.getInstance().killAllTimers();
 //        }
-//      }
-//
-//    });
+      }
+
+    });
   }
 
 }
